eb31a9c8417c757990facff2578b910f1c4076d5
Muhamed-ibrahim07 <mmohdibi@gmail.com>
2025-01-04 15:21:55 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,91 @@
+const gamePattern = [];
+let userClickedPattern = [];
+
+const buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  
+  userClickedPattern = [];
+
+  var randomnum = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomnum];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+$('[class*="btn"]').on("click", function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+let gameStarted = false;
+let level = 0;
+
+$(document).keydown(function () {
+  if (!gameStarted) {
+    resetGame();
+    addLevel();
+    nextSequence();
+    gameStarted = true;
+  }
+});
+
+function addLevel() {
+  level++;
+  $("#level-title").text("Level " + level);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+
+    
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        addLevel();
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("failure");
+    playSound("wrong");
+
+    
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  gamePattern.length = 0;
+  level = 0;
+  gameStarted = false;
+}
+
+function resetGame() {
+  $("#level-title").text("Level 0");
+}
